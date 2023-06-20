ca4026c863689d8aa20e4ea9354fa175bfe7ef0b
SebastianMartel <martel.ssebastian@gmail.com>
2023-06-19 23:33:36 -0500

    SearchBar.styles advance

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import { connect, useDispatch } from 'react-redux';
-import { sortAllRecipes, filterAllRecipes } from '../../Redux/actions';
+import { useDispatch } from 'react-redux';
+import { sortAllRecipes } from '../../Redux/actions';
 
 import './Home.css'
 import styled from 'styled-components';
@@ -10,7 +10,7 @@ import Pagination from '../Pagination/Pagination';
 //__________________________________________________
 
 
-export function Home ( { searching, allRecipes, searchResults, currentAllRecipes, currentSearchResults, recipesPerPage, paginate, deleteSuccess } ) {
+export default function Home ( { searching, allRecipes, searchResults, currentAllRecipes, currentSearchResults, recipesPerPage, paginate, deleteSuccess } ) {
 
 
     const dispatch = useDispatch()
@@ -20,15 +20,16 @@ export function Home ( { searching, allRecipes, searchResults, currentAllRecipes
         dispatch(sortAllRecipes(event.target.value))
     }
 
-    const handleFilter = (event) => {
-        dispatch(filterAllRecipes(event.target.value))
-    }
-
 
     return (
         <div>
             <div className = 'headline'>
                 <h1>HOME</h1>
+                {
+                    true && (
+                        <div className = 'deleteSuccessMessage'>you have successfully deleted your recipe</div>
+                    )
+                }
                 <select onChange = {(event) => {handleOrder(event)}}>
                     <option disabled hidden>Sort by name</option>
                     <option value = 'A'>Name (A-Z)</option>
@@ -39,43 +40,12 @@ export function Home ( { searching, allRecipes, searchResults, currentAllRecipes
                     <option value = 'C'>More healthy</option>
                     <option value = 'D'>Less healthy</option>
                 </select>
-                <select onChange = {(event) => {handleFilter(event)}}>
-                    <option value = '' disabled hidden>Explore through diets</option>
-                    <option value = 'All'>All</option>
-                    <option value = 'gluten free'>gluten free</option>
-                    <option value = 'dairy free'>dairy free</option>
-                    <option value = 'lacto ovo vegetarian'>lacto ovo vegetarian</option>
-                    <option value = 'vegan'>vegan</option>
-                    <option value = 'paleolithic'>paleolithic</option>
-                    <option value = 'primal'>primal</option>
-                    <option value = 'whole 30'>whole 30</option>
-                    <option value = 'pescatarian'>pescatarian</option>
-                    <option value = 'ketogenic'>ketogenic</option>
-                    <option value = 'fodmap friendly'>fodmap friendly</option>
-                </select>
-                <select onChange = {(event) => {handleFilter(event)}}>
-                    <option value = '' disabled hidden>Filter by origin</option>
-                    <option value = 'DB'>Your own recipes</option>
-                    <option value = 'API'>By others</option>
-                </select>
-                <p>ALL {allRecipes?.length}</p>
-                <p>SEARCH {searchResults?.length}</p>
+                {/* <p>ALL {allRecipes?.length}</p>
+                <p>SEARCH {searchResults?.length}</p> */}
             </div>
-
-            {
-                deleteSuccess && (
-                    <p>you have successfully deleted your recipe</p>
-                )
-            }
             <CardBox searching = {searching} allRecipes = {allRecipes} searchResults = {searchResults} currentAllRecipes = {currentAllRecipes} currentSearchResults = {currentSearchResults}/>
             <Pagination searching = {searching} recipesPerPage = {recipesPerPage} totalAllRecipes = {allRecipes.length} totalSearchResults = {searchResults.length} paginate = {paginate}/>
 
         </div>
     )
 }
-
-
-export default connect(
-    null,
-    null
-)(Home)
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
+import { useDispatch } from 'react-redux'; // removed connect, think it's unnecessary
+import { filterAllRecipes } from '../../Redux/actions';
+
 
 import './NavBar.css';
 import styled from "styled-components";
 import SearchBar from "../SearchBar/SearchBar";
-import log2 from '../../Media/log2.png'
+import mainLogo from '../../Media/mainLogo.png'
 //__________________________________________________
 
 
@@ -16,13 +19,41 @@ const StyledNavLink = styled(NavLink)`
 
 export default function NavBar ( { setSearching } ) {
 
+
+    const dispatch = useDispatch()
+
+
+    const handleFilter = (event) => {
+        dispatch(filterAllRecipes(event.target.value))
+    }
+
+
     return (
         <div className = "menu">
             <StyledNavLink to = '/home'>
                 {/* <h1 className = 'title' onClick = {() => {setSearching(false)}}>N O V A</h1> */}
-                <img src = {log2}/>
+                <img src = {mainLogo}/>
             </StyledNavLink>
             <SearchBar setSearching = {setSearching}/>
+            <select onChange = {(event) => {handleFilter(event)}}>
+                    <option disabled hidden>Explore through diets</option>
+                    <option value = 'All'>All</option>
+                    <option value = 'gluten free'>gluten free</option>
+                    <option value = 'dairy free'>dairy free</option>
+                    <option value = 'lacto ovo vegetarian'>lacto ovo vegetarian</option>
+                    <option value = 'vegan'>vegan</option>
+                    <option value = 'paleolithic'>paleolithic</option>
+                    <option value = 'primal'>primal</option>
+                    <option value = 'whole 30'>whole 30</option>
+                    <option value = 'pescatarian'>pescatarian</option>
+                    <option value = 'ketogenic'>ketogenic</option>
+                    <option value = 'fodmap friendly'>fodmap friendly</option>
+                </select>
+                <select onChange = {(event) => {handleFilter(event)}}>
+                    <option value = '' disabled hidden>Filter by origin</option>
+                    <option value = 'DB'>Your own recipes</option>
+                    <option value = 'API'>By others</option>
+                </select>
             <NavLink to = '/form'>
                 <button className = 'createRecipe'>ADD YOUR RECIPE</button>
             </NavLink>
